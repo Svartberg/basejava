e450016e6aa2a0d8f78e12e7877a7b96ab7c1ea7
Andrey Konstantinov <valgrind.havok@gmail.com>
2021-05-20 11:20:38 +0300

    lesson result

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,58 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int storageSize = 0;
+    int storageLastValue = 0;
 
     void clear() {
+        for (int i = 0; i < storageSize; i++) {
+            storage[i] = null;
+        }
+        storageSize = 0;
+        storageLastValue = 0;
     }
 
     void save(Resume r) {
+        storageSize++;
+        if (storageSize < 10000) {
+            storage[storageLastValue] = r;
+            storageLastValue++;
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume getReturn = null;
+        for (int i = 0; i < storageSize; i++) {
+            if (storage[i].toString().equals(uuid))
+                getReturn = storage[i];
+        }
+        return getReturn;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storageSize; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                if (storageSize - i >= 0) {
+                    System.arraycopy(storage, i + 1, storage, i, storageSize - i);
+                    break;
+                }
+            }
+        }
+        storage[storageLastValue] = null;
+        storageSize--;
+        storageLastValue--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] getStorage = new Resume[storageSize];
+        System.arraycopy(storage, 0, getStorage, 0, storageSize);
+        return getStorage;
     }
 
     int size() {
-        return 0;
+        return storageSize;
     }
 }
