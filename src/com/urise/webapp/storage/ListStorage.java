f49ebbeea6ae32b6de8cdfc3f14da9b6179b890b
Andrey Konstantinov <valgrind.havok@gmail.com>
2021-06-14 11:05:08 +0300

    Result lesson 5.2

--- src/com/urise/webapp/storage/ListStorage.java
+++ src/com/urise/webapp/storage/ListStorage.java
@@ -11,7 +11,7 @@ public class ListStorage extends AbstractStorage {
 
 
     @Override
-    protected Object getSearchID(String uuid) {
+    protected Integer getSearchID(String uuid) {
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).getUuid().equals(uuid)) {
                 return i;
--- src/com/urise/webapp/storage/MapStorage.java
+++ src/com/urise/webapp/storage/MapStorage.java
@@ -2,40 +2,46 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapStorage extends AbstractStorage {
+
+    private Map<String, Resume> map = new HashMap<>();
+
     @Override
     protected Object getSearchID(String uuid) {
-        return null;
+        return uuid;
     }
 
     @Override
-    protected void doSave(Resume r, Object searchID) {
-
+    protected void doSave(Resume r, Object uuid) {
+        map.put((String) uuid, r);
     }
 
     @Override
-    protected void doUpdate(Resume r, Object searchID) {
-
+    protected void doUpdate(Resume r, Object uuid) {
+        map.put((String) uuid, r);
     }
 
     @Override
-    protected boolean isExist(Object searchID) {
-        return false;
+    protected boolean isExist(Object uuid) {
+        return map.containsKey((String) uuid);
     }
 
     @Override
-    protected void doDelete(Object searchKey) {
-
+    protected void doDelete(Object uuid) {
+        map.remove((String) uuid);
     }
 
     @Override
-    protected Resume doGet(Object searchKey) {
-        return null;
+    protected Resume doGet(Object uuid) {
+        return map.get((String) uuid);
     }
 
     @Override
     public void clear() {
-
+        map.clear();
     }
 
     @Override
@@ -45,6 +51,6 @@ public class MapStorage extends AbstractStorage {
 
     @Override
     public int size() {
-        return 0;
+        return map.size();
     }
 }
