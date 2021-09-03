9623877235d0d29431a558469573a5c1101c4ebf
Svartberg <valgrind.havok@gmail.com>
2021-09-03 22:00:34 +0300

    Lesson 13 result

---
+++ src/com/urise/webapp/sql/ExceptionUtil.java
@@ -0,0 +1,22 @@
+package com.urise.webapp.sql;
+
+import org.postgresql.util.PSQLException;
+import com.urise.webapp.exception.ExistStorageException;
+import com.urise.webapp.exception.StorageException;
+
+import java.sql.SQLException;
+
+public class ExceptionUtil {
+    private ExceptionUtil() {
+    }
+
+    public static StorageException convertException(SQLException e) {
+        if (e instanceof PSQLException) {
+
+            if (e.getSQLState().equals("23505")) {
+                return new ExistStorageException(null);
+            }
+        }
+        return new StorageException(e);
+    }
+}
--- test/com/urise/webapp/storage/AbstractStorageTest.java
+++ test/com/urise/webapp/storage/AbstractStorageTest.java
@@ -1,13 +1,16 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.Config;
 import com.urise.webapp.exception.ExistStorageException;
 import com.urise.webapp.exception.NotExistStorageException;
 import com.urise.webapp.model.*;
 
 import java.io.File;
-import java.time.Month;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,14 +20,14 @@ import static org.junit.Assert.assertTrue;
 
 public abstract class AbstractStorageTest {
 
-    protected static final File STORAGE_DIR = new File("E:\\JavaLearn\\basejava\\src\\storage");
+    protected static final File STORAGE_DIR = Config.get().getStorageDir();
 
     protected Storage storage;
 
-    private static final String UUID_1 = "uuid1";
-    private static final String UUID_2 = "uuid2";
-    private static final String UUID_3 = "uuid3";
-    private static final String UUID_4 = "uuid4";
+    private static final String UUID_1 = UUID.randomUUID().toString();;
+    private static final String UUID_2 = UUID.randomUUID().toString();;
+    private static final String UUID_3 = UUID.randomUUID().toString();;
+    private static final String UUID_4 = UUID.randomUUID().toString();;
 
     private static final Resume R1;
     private static final Resume R2;
@@ -39,7 +42,7 @@ public abstract class AbstractStorageTest {
         R3 = new Resume(UUID_3, "Name3");
         R4 = new Resume(UUID_4, "Name4");
 
-        R1.addContact(ContactType.MAIL, "mail1@ya.ru");
+        /*R1.addContact(ContactType.MAIL, "mail1@ya.ru");
         R1.addContact(ContactType.PHONE, "11111");
         R1.addSection(TypeSection.OBJECTIVE, new TextSection("Objective1"));
         R1.addSection(TypeSection.PERSONAL, new TextSection("Personal data"));
@@ -61,7 +64,7 @@ public abstract class AbstractStorageTest {
         R1.addSection(TypeSection.EXPERIENCE,
                 new HistorySection(
                         new Organization("Organization2", "http://Organization2.ru",
-                                new Organization.Position(2015, Month.JANUARY, "position1", "content1"))));
+                                new Organization.Position(2015, Month.JANUARY, "position1", "content1"))));*/
     }
 
 
@@ -132,7 +135,9 @@ public abstract class AbstractStorageTest {
     public void getAllSorted() throws Exception {
         List<Resume> list = storage.getAllSorted();
         assertEquals(3, list.size());
-        assertEquals(list, Arrays.asList(R1, R2, R3));
+        List<Resume> sortedResumes = Arrays.asList(R1, R2, R3);
+        Collections.sort(sortedResumes);
+        assertEquals(list, sortedResumes);
     }
 
     @Test(expected = NotExistStorageException.class)
